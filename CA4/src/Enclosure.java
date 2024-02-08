54469e6ddad64c2cc14b2401a6a8123efe058b72
pipipipingu <158144058+pipipipingu@users.noreply.github.com>
2024-02-09 02:04:17 +0800

    Update Enclosure.java

--- CA4/src/Enclosure.java
+++ CA4/src/Enclosure.java
@@ -3,21 +3,78 @@ public class Enclosure {
     // Field Descriptors
 
     private String name;
-    private double area;
+    private int area;
     private ArrayList<Animal> animals;
 
     // constructor
-    public Enclosure(String name, double area) {
+    public Enclosure(String name, int area, ArrayList<Animal> animals) {
+        this.name = name;
+        this.area = area;
+        this.animals = animals;
     }
 
     // method descriptors
-    public int countAnimals() { return 0; }
-    public double getUtilisedArea() { return 0; }
-    public double getUtilisedAreaPercentage() { return 0; }
+    public int countAnimals() {
+        return animals.size();
+    }
 
-    public int countSpecies() { return 0; }
+    public int getUtilisedArea() {
+        int totalUtilisedArea = 0;
+        for (Animal animal : animals) {
+            totalUtilisedArea += animal.getAreaNeeded();
+        }
+        return totalUtilisedArea;
+    }
 
-    public boolean addAnimal(Animal animal) { return false; }
+    public double getUtilisedAreaPercentage() {
+        double areaUsed = getUtilisedArea();
+        double percentage = areaUsed / area;
+        return percentage;
+    }
 
-    // other methods add below as needed
+    public int countSpecies(String type) {
+        int countSpecies = 0;
+        for (Animal animal : animals) {
+            if (animal.getSpecies().equals(type)) {
+                countSpecies++;
+            }
+        }
+        return countSpecies;
+    }
+
+    public boolean addAnimal(Animal animal) {
+        if ((area - getUtilisedArea()) >= animal.getAreaNeeded()) {
+            animals.add(animal);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // getter and setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // getter and setter for area
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    // getter and setter for animals
+    public ArrayList<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(ArrayList<Animal> animals) {
+        this.animals = animals;
+    }
 }
