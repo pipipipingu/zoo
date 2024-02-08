865c33f3f6420e0455366b0ae0e7c80ba525bd29
pipipipingu <158144058+pipipipingu@users.noreply.github.com>
2024-02-09 02:05:04 +0800

    Update Zoo.java

--- CA4/src/Zoo.java
+++ CA4/src/Zoo.java
@@ -7,16 +7,48 @@ public class Zoo {
 
     // constructor
     public Zoo(String name, String description, ArrayList<Enclosure> enclosures){
+        this.name = name;
+        this.description = description;
+        this.enclosures = enclosures;
     }
 
     // method descriptors
-    public double getTotalEnclosureArea() {
-        return 0;
+    public int getTotalEnclosureArea() {
+        int totalEnclosureArea = 0;
+        for (Enclosure enclosure : enclosures) {
+            totalEnclosureArea += enclosure.getArea();
+        }
+        return totalEnclosureArea;
     }
 
-    public int countEnclosures() { return 0; }
+    public int countEnclosures() {
+        return enclosures.size();
+    }
 
-    // other methods add below as needed
+    // getter and setter for name
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // getter and setter for description
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription( String description) {
+        this.description = description;
+    }
+
+    // getter and setter for enclosure
+    public ArrayList<Enclosure> getEnclosures() {
+        return enclosures;
+    }
+
+    public void setEnclosures (ArrayList<Enclosure> enclosures) {
+        this.enclosures = enclosures;
+    }
 }
-
