e966e7e9d641cd5d8e0f0ca54460472fc73e098f
pipipipingu <158144058+pipipipingu@users.noreply.github.com>
2024-02-09 02:03:32 +0800

    Update Animal.java

--- CA4/src/Animal.java
+++ CA4/src/Animal.java
@@ -2,25 +2,53 @@ public class Animal {
     //Field descriptors
     private String species;
     private Enclosure enclosure;
-    private double areaNeeded;
+    private int areaNeeded;
 
     // constructor
-    public Animal(String species, double areaNeeded) {
-
+    public Animal(String species, Enclosure enclosure, int areaNeeded) {
+        this.species = species;
+        this.enclosure = enclosure;
+        this.areaNeeded = areaNeeded;
     }
 
+
     // method descriptors
-    public boolean hasCompanion() { return false;}
+    public boolean hasCompanion() {
+        int countSpecies = 0;
+        for (Animal animal : enclosure.getAnimals()) {
+            if (animal.getSpecies().equals(species)) {
+                countSpecies++;
+            }
+        }
+        return countSpecies >=1;
+    }
+
+    // getter and setter for species
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
 
     // getter and setter for Enclosure
     public Enclosure getEnclosure() {
-        return null;
+        return enclosure;
     }
 
     public void setEnclosure(Enclosure enclosure) {
-
+        this.enclosure = enclosure;
     }
 
-    // other methods add below when needed
-}
 
+    // getter and setter for areaNeeded
+    public int getAreaNeeded() {
+        return areaNeeded;
+    }
+
+    public void setAreaNeeded() {
+        this.areaNeeded = areaNeeded;
+    }
+}
